20c5ffd87a58c4e408cf6c14f4b21121457b6a93
Alexander Schott <alexschott87@gmail.com>
2017-10-31 22:00:59 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,19 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+# Two different representations...
+# 1)           2)
+#     | X |        | X |
+#  ---|---|---  -----------
+#     | O |        | O |
+#  ---|---|---  -----------
+#     |   |        |   |
+
+def display_board
+    i = 0
+    5.times do
+        if i % 2
+            puts "---|---|---"
+        else
+            puts "   |   |   "
+        i += 1
+    end
+end
